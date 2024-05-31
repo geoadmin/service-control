abbddb71625dd08af22378d62264a7320af387d8
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2024-05-31 13:40:42 +0200

    PB-554 Install debug toolbar and django extensions

--- app/config/settings_dev.py
+++ app/config/settings_dev.py
@@ -6,3 +6,10 @@ env = environ.Env()
 
 DEBUG = env.bool('DEBUG', True)
 
+if DEBUG:
+    INSTALLED_APPS += ['django_extensions', 'debug_toolbar']
+
+if DEBUG:
+    MIDDLEWARE = [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    ] + MIDDLEWARE
