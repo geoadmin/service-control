8c9947cb38942fc104ae0f6372203d722c1ed4db
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2024-06-10 11:57:49 +0200

    PB-544 Add the very first basic test

---
+++ app/controller/tests/test_provider_model.py
@@ -0,0 +1,22 @@
+from faker import Faker
+from django.test import TestCase
+
+from controller.models import Provider
+
+fake = Faker()
+
+
+class ProviderTestCase(TestCase):
+
+    def test_simple_model_creation(self):
+        name = fake.company()
+        prefix = fake.company_suffix()
+        Provider.objects.create(name=name, prefix=prefix)
+
+        providers = Provider.objects.all()
+
+        self.assertEqual(len(providers), 1)
+
+        provider = Provider.objects.last()
+        self.assertEqual(provider.name, name)
+        self.assertEqual(provider.prefix, prefix)
