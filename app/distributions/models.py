0b5d926745e65ba311c11920f2eef3bafdaccc03
Alexander Steiner <alexander.steiner@swisstopo.ch>
2024-09-16 11:52:54 +0200

    PB-902 Add context to texts marked for translation
    Removed the MetaClass.verbose_name(_plural) because here they merely write out what Django does already by itself (take the class name as lower case, add an "s" for plural).

--- app/distributions/models.py
+++ app/distributions/models.py
@@ -1,43 +1,40 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import pgettext_lazy as _
 
 
 class Attribution(models.Model):
 
-    class Meta:
-        verbose_name = _("attribution")
-        verbose_name_plural = _("attributions")
+    _context = "Attribution Model"
 
     def __str__(self) -> str:
         return str(self.name_en)
 
-    name_de = models.CharField(_("Name (German)"))
-    name_fr = models.CharField(_("Name (French)"))
-    name_en = models.CharField(_("Name (English)"))
-    name_it = models.CharField(_("Name (Italian)"), blank=True)
-    name_rm = models.CharField(_("Name (Romansh)"), blank=True)
+    name_de = models.CharField(_(_context, "Name (German)"))
+    name_fr = models.CharField(_(_context, "Name (French)"))
+    name_en = models.CharField(_(_context, "Name (English)"))
+    name_it = models.CharField(_(_context, "Name (Italian)"), blank=True)
+    name_rm = models.CharField(_(_context, "Name (Romansh)"), blank=True)
 
-    description_de = models.CharField(_("Description (German)"))
-    description_fr = models.CharField(_("Description (French)"))
-    description_en = models.CharField(_("Description (English)"))
-    description_it = models.CharField(_("Description (Italian)"), blank=True)
-    description_rm = models.CharField(_("Description (Romansh)"), blank=True)
+    description_de = models.CharField(_(_context, "Description (German)"))
+    description_fr = models.CharField(_(_context, "Description (French)"))
+    description_en = models.CharField(_(_context, "Description (English)"))
+    description_it = models.CharField(_(_context, "Description (Italian)"), blank=True)
+    description_rm = models.CharField(_(_context, "Description (Romansh)"), blank=True)
 
     provider = models.ForeignKey("provider.Provider", on_delete=models.CASCADE)
 
 
 class Dataset(models.Model):
 
-    class Meta:
-        verbose_name = _("dataset")
-        verbose_name_plural = _("datasets")
+    _context = "Dataset Model"
 
     def __str__(self) -> str:
         return str(self.slug)
 
-    slug = models.SlugField(_("Slug"))
-    created = models.DateTimeField(_("Created"), auto_now_add=True)
-    updated = models.DateTimeField(_("Updated"), auto_now=True)
+
+    slug = models.SlugField(_(_context, "Slug"))
+    created = models.DateTimeField(_(_context, "Created"), auto_now_add=True)
+    updated = models.DateTimeField(_(_context, "Updated"), auto_now=True)
 
     provider = models.ForeignKey("provider.Provider", on_delete=models.CASCADE)
     attribution = models.ForeignKey(Attribution, on_delete=models.CASCADE)
--- app/provider/models.py
+++ app/provider/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import pgettext_lazy as _
 
 
 class Provider(models.Model):
 
-    class Meta:
-        verbose_name = _("provider")
-        verbose_name_plural = _("providers")
+    _context = "Provider model"
 
     def __str__(self) -> str:
         return str(self.name_en)
@@ -15,14 +13,14 @@ class Provider(models.Model):
     Note: The "blank=False" for a model field doesn't prevent DB changes.
           It only has an effect on form validation.
     '''
-    name_de = models.CharField(_("Name (German)"))
-    name_fr = models.CharField(_("Name (French)"))
-    name_en = models.CharField(_("Name (English)"))
-    name_it = models.CharField(_("Name (Italian)"), blank=True)
-    name_rm = models.CharField(_("Name (Romansh)"), blank=True)
+    name_de = models.CharField(_(_context, "Name (German)"))
+    name_fr = models.CharField(_(_context, "Name (French)"))
+    name_en = models.CharField(_(_context, "Name (English)"))
+    name_it = models.CharField(_(_context, "Name (Italian)"), blank=True)
+    name_rm = models.CharField(_(_context, "Name (Romansh)"), blank=True)
 
-    acronym_de = models.CharField(_("Acronym (German)"))
-    acronym_fr = models.CharField(_("Acronym (French)"))
-    acronym_en = models.CharField(_("Acronym (English)"))
-    acronym_it = models.CharField(_("Acronym (Italian)"), blank=True)
-    acronym_rm = models.CharField(_("Acronym (Romansh)"), blank=True)
+    acronym_de = models.CharField(_(_context, "Acronym (German)"))
+    acronym_fr = models.CharField(_(_context, "Acronym (French)"))
+    acronym_en = models.CharField(_(_context, "Acronym (English)"))
+    acronym_it = models.CharField(_(_context, "Acronym (Italian)"), blank=True)
+    acronym_rm = models.CharField(_(_context, "Acronym (Romansh)"), blank=True)
