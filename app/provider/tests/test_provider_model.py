abe0de1d3c7ea05c694a297a510d0430e4a845fd
Alexander Steiner <alexander.steiner@swisstopo.ch>
2024-09-18 17:16:49 +0200

    PB-902 Make optional fields nullable in model Provider

--- app/provider/tests/test_provider_model.py
+++ app/provider/tests/test_provider_model.py
@@ -1,12 +1,13 @@
 from provider.models import Provider
 
+from django.db import IntegrityError
 from django.forms import ModelForm
 from django.test import TestCase
 
 
 class ProviderTestCase(TestCase):
 
-    def test_model_creation_as_expected_when_all_fields_defined(self):
+    def test_object_created_in_db_with_all_fields_defined(self):
         name_de = "Bundesamt für Umwelt"
         name_fr = "Office fédéral de l'environnement"
         name_en = "Federal Office for the Environment"
@@ -46,6 +47,50 @@ class ProviderTestCase(TestCase):
         self.assertEqual(provider.acronym_en, acronym_en)
         self.assertEqual(provider.acronym_it, acronym_it)
         self.assertEqual(provider.acronym_rm, acronym_rm)
+
+    def test_object_created_in_db_with_optional_fields_null(self):
+        name_de = "Bundesamt für Umwelt"
+        name_fr = "Office fédéral de l'environnement"
+        name_en = "Federal Office for the Environment"
+        name_it = None
+        name_rm = None
+        acronym_de = "BAFU"
+        acronym_fr = "OFEV"
+        acronym_en = "FOEN"
+        acronym_it = None
+        acronym_rm = None
+        Provider.objects.create(
+            name_de=name_de,
+            name_fr=name_fr,
+            name_en=name_en,
+            name_it=name_it,
+            name_rm=name_rm,
+            acronym_de=acronym_de,
+            acronym_fr=acronym_fr,
+            acronym_en=acronym_en,
+            acronym_it=acronym_it,
+            acronym_rm=acronym_rm,
+        )
+
+        providers = Provider.objects.all()
+
+        self.assertEqual(len(providers), 1)
+
+        provider = Provider.objects.last()
+        self.assertEqual(provider.name_de, name_de)
+        self.assertEqual(provider.name_fr, name_fr)
+        self.assertEqual(provider.name_en, name_en)
+        self.assertEqual(provider.name_it, name_it)
+        self.assertEqual(provider.name_rm, name_rm)
+
+        self.assertEqual(provider.acronym_de, acronym_de)
+        self.assertEqual(provider.acronym_fr, acronym_fr)
+        self.assertEqual(provider.acronym_en, acronym_en)
+        self.assertEqual(provider.acronym_it, acronym_it)
+        self.assertEqual(provider.acronym_rm, acronym_rm)
+
+    def test_raises_exception_when_creating_db_object_with_mandatory_field_null(self):
+        self.assertRaises(IntegrityError, Provider.objects.create, name_de=None)
 
     def test_form_valid_for_blank_optional_field(self):
 
