accec330e4f629fd1bde3ebf0fea73dbb7dc2b74
Alexander Steiner <alexander.steiner@swisstopo.ch>
2024-09-25 16:58:53 +0200

    PB-903 Take the HTTP header "Accept-Language" into account

--- app/provider/api.py
+++ app/provider/api.py
@@ -1,7 +1,9 @@
 from ninja import Router
+from utils.header import extract_lang
 from utils.translation import LanguageCode
 from utils.translation import get_translation
 
+from django.http import HttpRequest
 from django.shortcuts import get_object_or_404
 
 from .models import Provider
@@ -12,7 +14,57 @@ router = Router()
 
 
 @router.get("/providers/{provider_id}", response={200: ProviderSchema}, exclude_none=True)
-def provider(request, provider_id: str, lang: LanguageCode = LanguageCode.ENGLISH):
+def provider(request: HttpRequest, provider_id: str, lang: LanguageCode | None = None):
+    """
+    Get the provider with the given ID, return translatable fields in the given language.
+
+    Example: If German ("de") is set as the language of choice, fields "name" and
+             "acronym" would take the value of the corresponding translation.
+
+                {
+                    "id": "1",
+                    "name": "German",
+                    "name_translations": {
+                        "de": "German",
+                        "fr": "French",
+                        "en": "English",
+                        "it": "Italian",
+                        "rm": "Romansh",
+                    },
+                    "acronym": "DE",
+                    "acronym_translations": {
+                        "de": "DE",
+                        "fr": "FR",
+                        "en": "EN",
+                        "it": "IT",
+                        "rm": "RM",
+                    }
+                }
+
+    The language can be set via
+
+        1. Query parameter. For example: "lang=de"
+        2. Header "Accept-Language". For example: "Accept-Language: de"
+
+    To consider for the language setting:
+
+        - If both query param and header are set, the query param is taken.
+        - The valid languages are: "de", "fr", "en", "it", "rm"
+        - If no language is specified, English is taken as the default.
+
+    To consider for the header "Accept-Language":
+
+        - For multiple languages, the valid language with the largest q-factor
+          is taken (Example: "de;q=0.7, rm;q=0.8" --> "rm")
+        - Subtags in the header are ignored. So "en-US" is interpreted as "en".
+        - Wildcards ("*") are ignored.
+    """
+    if not lang:
+        if "Accept-Language" in request.headers:
+            lang_header = request.headers["Accept-Language"]
+            lang = extract_lang(lang_header)
+        else:
+            lang = LanguageCode.ENGLISH
 
     provider_object = get_object_or_404(Provider, id=provider_id)
     schema = ProviderSchema(
--- app/provider/tests/test_api.py
+++ app/provider/tests/test_api.py
@@ -51,7 +51,7 @@ class ApiTestCase(TransactionTestCase):
             }
         }
 
-    def test_get_provider_returns_provider_with_given_language(self):
+    def test_get_provider_returns_provider_with_language_from_query(self):
 
         provider = {
             "name_de": "Bundesamt für Umwelt",
@@ -99,7 +99,6 @@ class ApiTestCase(TransactionTestCase):
         assert response.status_code == 404
         assert response.data == {"detail": "Not Found"}
 
-
     def test_get_provider_skips_translations_that_are_not_available(self):
 
         provider = {
@@ -132,3 +131,162 @@ class ApiTestCase(TransactionTestCase):
             }
         }
 
+    def test_get_provider_returns_provider_with_language_from_header(self):
+
+        provider = {
+            "name_de": "Bundesamt für Umwelt",
+            "name_fr": "Office fédéral de l'environnement",
+            "name_en": "Federal Office for the Environment",
+            "acronym_de": "BAFU",
+            "acronym_fr": "OFEV",
+            "acronym_en": "FOEN",
+        }
+        Provider.objects.create(**provider)
+
+        client = TestClient(router)
+        response = client.get("/providers/1", headers={"Accept-Language": "de"})
+
+        assert response.status_code == 200
+        assert response.data == {
+            "id": "1",
+            "name": "Bundesamt für Umwelt",
+            "name_translations": {
+                "de": "Bundesamt für Umwelt",
+                "fr": "Office fédéral de l'environnement",
+                "en": "Federal Office for the Environment",
+            },
+            "acronym": "BAFU",
+            "acronym_translations": {
+                "de": "BAFU",
+                "fr": "OFEV",
+                "en": "FOEN",
+            }
+        }
+
+    def test_get_provider_returns_provider_with_language_from_query_param_even_if_header_set(self):
+
+        provider = {
+            "name_de": "Bundesamt für Umwelt",
+            "name_fr": "Office fédéral de l'environnement",
+            "name_en": "Federal Office for the Environment",
+            "acronym_de": "BAFU",
+            "acronym_fr": "OFEV",
+            "acronym_en": "FOEN",
+        }
+        Provider.objects.create(**provider)
+
+        client = TestClient(router)
+        response = client.get("/providers/1?lang=fr", headers={"Accept-Language": "de"})
+
+        assert response.status_code == 200
+        assert response.data == {
+            "id": "1",
+            "name": "Office fédéral de l'environnement",
+            "name_translations": {
+                "de": "Bundesamt für Umwelt",
+                "fr": "Office fédéral de l'environnement",
+                "en": "Federal Office for the Environment",
+            },
+            "acronym": "OFEV",
+            "acronym_translations": {
+                "de": "BAFU",
+                "fr": "OFEV",
+                "en": "FOEN",
+            }
+        }
+
+    def test_get_provider_returns_provider_with_default_language_if_header_empty(self):
+
+        provider = {
+            "name_de": "Bundesamt für Umwelt",
+            "name_fr": "Office fédéral de l'environnement",
+            "name_en": "Federal Office for the Environment",
+            "acronym_de": "BAFU",
+            "acronym_fr": "OFEV",
+            "acronym_en": "FOEN",
+        }
+        Provider.objects.create(**provider)
+
+        client = TestClient(router)
+        response = client.get("/providers/1", headers={"Accept-Language": ""})
+
+        assert response.status_code == 200
+        assert response.data == {
+            "id": "1",
+            "name": "Federal Office for the Environment",
+            "name_translations": {
+                "de": "Bundesamt für Umwelt",
+                "fr": "Office fédéral de l'environnement",
+                "en": "Federal Office for the Environment",
+            },
+            "acronym": "FOEN",
+            "acronym_translations": {
+                "de": "BAFU",
+                "fr": "OFEV",
+                "en": "FOEN",
+            }
+        }
+
+    def test_get_provider_returns_provider_with_first_known_language_from_header(self):
+
+        provider = {
+            "name_de": "Bundesamt für Umwelt",
+            "name_fr": "Office fédéral de l'environnement",
+            "name_en": "Federal Office for the Environment",
+            "acronym_de": "BAFU",
+            "acronym_fr": "OFEV",
+            "acronym_en": "FOEN",
+        }
+        Provider.objects.create(**provider)
+
+        client = TestClient(router)
+        response = client.get("/providers/1", headers={"Accept-Language": "cn, *, de-DE, en"})
+
+        assert response.status_code == 200
+        assert response.data == {
+            "id": "1",
+            "name": "Bundesamt für Umwelt",
+            "name_translations": {
+                "de": "Bundesamt für Umwelt",
+                "fr": "Office fédéral de l'environnement",
+                "en": "Federal Office for the Environment",
+            },
+            "acronym": "BAFU",
+            "acronym_translations": {
+                "de": "BAFU",
+                "fr": "OFEV",
+                "en": "FOEN",
+            }
+        }
+
+    def test_get_provider_returns_provider_with_language_having_largest_q_factor(self):
+
+        provider = {
+            "name_de": "Bundesamt für Umwelt",
+            "name_fr": "Office fédéral de l'environnement",
+            "name_en": "Federal Office for the Environment",
+            "acronym_de": "BAFU",
+            "acronym_fr": "OFEV",
+            "acronym_en": "FOEN",
+        }
+        Provider.objects.create(**provider)
+
+        client = TestClient(router)
+        response = client.get("/providers/1", headers={"Accept-Language": "fr;q=1.0, de;q=0.8"})
+
+        assert response.status_code == 200
+        assert response.data == {
+            "id": "1",
+            "name": "Office fédéral de l'environnement",
+            "name_translations": {
+                "de": "Bundesamt für Umwelt",
+                "fr": "Office fédéral de l'environnement",
+                "en": "Federal Office for the Environment",
+            },
+            "acronym": "OFEV",
+            "acronym_translations": {
+                "de": "BAFU",
+                "fr": "OFEV",
+                "en": "FOEN",
+            }
+        }
