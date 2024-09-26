617d4d6659ef436aac012ba17c6a8042b466244b
Alexander Steiner <alexander.steiner@swisstopo.ch>
2024-09-26 18:14:56 +0200

    PB-903 Pass the valid languages and return None if value doesn't match

--- app/utils/header.py
+++ app/utils/header.py
@@ -1,30 +1,23 @@
 from typing import Final
 
-from .language import LanguageCode
-
 # to prevent possible denial of service or memory exhaustion attacks
 ACCEPT_LANGUAGE_HEADER_MAX_LENGTH: Final = 500
-DEFAULT_LANGUAGE: Final = LanguageCode.ENGLISH
 
 
-def extract_lang(accept_lang_header: str) -> LanguageCode:
+def extract_lang(accept_lang_header: str, valid_langs: list[str]) -> str | None:
     """
-    Extract the first supported language from the HTTP header "Accept-Language".
+    Extract the first valid language from the HTTP header "Accept-Language".
 
-    Examples:
-
-        "fr;q=0.5, de-CH;q=0.7" --> "fr"
-        "ru, de-CH,*"           --> "de"
-        "zh-CN, es-ES, li"      --> "en"
+    Return None if there is no valid language or if the header is too long.
     """
     if len(accept_lang_header) > ACCEPT_LANGUAGE_HEADER_MAX_LENGTH:
-        return DEFAULT_LANGUAGE
+        return None
 
     for lang_with_q in accept_lang_header.split(","):
         stripped = lang_with_q.strip()
         lang_with_subtag = stripped.split(";", maxsplit=1)[0]
         lang = lang_with_subtag.split("-", maxsplit=1)[0]
-        if lang in LanguageCode:
-            return LanguageCode(lang)
+        if lang in valid_langs:
+            return lang
 
-    return DEFAULT_LANGUAGE
+    return None
