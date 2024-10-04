b19d375f410af24d38de0ea6943705deae390577
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2024-10-04 16:08:29 +0200

    PB-897 Fix the checker URL
    The checker URL wasn't reachable anymore after changes to the URL
    structure

--- app/config/api.py
+++ app/config/api.py
@@ -1,11 +1,10 @@
 from ninja import NinjaAPI
 from provider.api import router as provider_router
 
-root = NinjaAPI()
 api = NinjaAPI()
 
 
-@root.get('/checker')
+@api.get('/checker')
 def checker(request):
     return {"success": True, "message": " OK"}
 
