f41f1395c8b7116bfa60fcce2a1a705aad0aec4f
Alexander Steiner <alexander.steiner@swisstopo.ch>
2024-10-10 17:38:10 +0200

    PB-1045 Add User model

--- app/access/models.py
+++ app/access/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import pgettext_lazy as _
 
-# Create your models here.
+
+class User(models.Model):
+
+    _context = "User model"
+
+    def __str__(self) -> str:
+        return str(self.first_name) + str(self.last_name)
+
+    first_name = models.CharField(_(_context, "First name"))
+    last_name = models.CharField(_(_context, "Last name"))
+    email = models.EmailField(_(_context, "Email"))
+
+    provider = models.ForeignKey("provider.Provider", on_delete=models.CASCADE)
