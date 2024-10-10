cce18da475f300a271273ea2289ea01a3ffc2de3
Alexander Steiner <alexander.steiner@swisstopo.ch>
2024-10-10 17:40:52 +0200

    PB-1045 Register the router of app "access" in the Ninja API

--- app/config/api.py
+++ app/config/api.py
@@ -1,8 +1,10 @@
+from access.api import router as access_router
 from distributions.api import router as distributions_router
 from ninja import NinjaAPI
 from provider.api import router as provider_router
 
 api = NinjaAPI()
 
-api.add_router("providers", provider_router)
-api.add_router("distributions", distributions_router)
+api.add_router("", provider_router)
+api.add_router("", distributions_router)
+api.add_router("", access_router)
