2e9db446410a89137297ae8334d6d17761d689c0
Alexander Steiner <alexander.steiner@swisstopo.ch>
2024-10-14 15:05:24 +0200

    PB-1045 Add tests for new User model
    Of interest here because it demonstrates that the email is only validated in forms, not when writing to the DB. We check that currently only on end point level using Ninja schemas.

---
+++ app/access/tests/test_models.py
@@ -0,0 +1,42 @@
+import pytest
+from access.models import User
+from provider.models import Provider
+
+from django.forms import ValidationError
+
+
+@pytest.mark.django_db
+class TestUser:
+
+    def test_user_stored_as_expected_for_valid_input(self):
+        provider = Provider.objects.create()
+        model_fields = {
+            "first_name": "Jeffrey",
+            "last_name": "Lebowski",
+            "email": "dude@bowling.com",
+            "provider": provider,
+        }
+
+        actual = User.objects.create(**model_fields)
+
+        assert actual.first_name == "Jeffrey"
+        assert actual.last_name == "Lebowski"
+        assert actual.email == "dude@bowling.com"
+        assert actual.provider == provider
+
+    def test_user_stores_user_even_with_invalid_email(self):
+        # Demonstrates that an EmailField only ensures form validation, not model validation.
+        provider = Provider.objects.create()
+        model_fields = {
+            "first_name": "Jeffrey",
+            "last_name": "Lebowski",
+            "email": "dude_at_bowling_dot_com",
+            "provider": provider,
+        }
+
+        actual = User.objects.create(**model_fields)
+
+        assert actual.first_name == "Jeffrey"
+        assert actual.last_name == "Lebowski"
+        assert actual.email == "dude_at_bowling_dot_com"
+        assert actual.provider == provider
