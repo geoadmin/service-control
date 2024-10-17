01ea258a59b88299d64121566547f6e4f8fb9f10
Alexander Steiner <alexander.steiner@swisstopo.ch>
2024-10-17 14:05:06 +0200

    PB-1045 Keep numerical ID internally, use unique username internally

--- app/access/models.py
+++ app/access/models.py
@@ -12,7 +12,7 @@ class User(models.Model):
     def __str__(self) -> str:
         return str(self.first_name) + str(self.last_name)
 
-    username = models.CharField(_(_context, "User name"), primary_key=True)
+    username = models.CharField(_(_context, "User name"), unique=True)
     first_name = models.CharField(_(_context, "First name"))
     last_name = models.CharField(_(_context, "Last name"))
     email = models.EmailField(_(_context, "Email"))
