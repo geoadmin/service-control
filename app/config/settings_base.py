a47d85f4c6e1ca7c812138e28e4c70ddd9184128
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2024-12-10 11:53:59 +0100

    Add SECURE_PROXY_SSL_HEADER configuration

--- app/config/settings_base.py
+++ app/config/settings_base.py
@@ -25,6 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY', default=None)
 
+# SECURITY:
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ('HTTP_CLOUDFRONT_FORWARDED_PROTO', 'https')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
